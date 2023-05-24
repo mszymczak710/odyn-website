159455bac7223efcd93ab832e996dbfe63968f8d
Mateusz Szymczak <mszymczak710@o2.pl>
2023-05-24 15:54:50 +0200

    refactor finish-time component

--- website/src/app/components/finish-time/finish-time.component.ts
+++ website/src/app/components/finish-time/finish-time.component.ts
@@ -1,16 +1,13 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-finish-time',
   templateUrl: './finish-time.component.html',
   styleUrls: ['./finish-time.component.scss'],
 })
-export class FinishTimeComponent {
-  date: any;
-  now: any;
-  targetDate: any = new Date('2023/06/01');
-  targetTime: any = this.targetDate.getTime();
-  difference: number;
+export class FinishTimeComponent implements AfterViewInit {
+  targetDate: any = new Date('2023-06-01T13:00:00');
+  timeDifference: number;
   @ViewChild('weeks', { static: true }) weeks: ElementRef;
   @ViewChild('days', { static: true }) days: ElementRef;
   @ViewChild('hours', { static: true }) hours: ElementRef;
@@ -19,57 +16,37 @@ export class FinishTimeComponent {
 
   ngAfterViewInit() {
     setInterval(() => {
-      this.tickTock();
-      this.difference = this.targetTime - this.now;
-      this.difference = this.difference / (1000 * 60 * 60 * 24 * 7);
-
-      if (!isNaN(this.weeks.nativeElement.innerText)) {
-        Math.floor(this.difference) < 10
-          ? (this.weeks.nativeElement.innerText =
-              '0' + Math.floor(this.difference))
-          : (this.weeks.nativeElement.innerText = Math.floor(this.difference));
-      } else {
-        this.weeks.nativeElement.innerHTML = `<img src="https://i.gifer.com/VAyR.gif" />`;
-      }
-
-      if (!isNaN(this.days.nativeElement.innerText)) {
-        Math.floor(this.difference / 7) < 10
-          ? (this.days.nativeElement.innerText =
-              '0' + Math.floor(this.difference / 7))
-          : (this.days.nativeElement.innerText = Math.floor(
-              this.difference / 7
-            ));
-      } else {
-        this.days.nativeElement.innerHTML = `<img src="https://i.gifer.com/VAyR.gif" />`;
-      }
+      this.calculateCountdown();
+      this.updateCountdown();
     }, 1000);
   }
 
-  tickTock() {
-    this.date = new Date();
-    this.now = this.date.getTime();
+  calculateCountdown() {
+    const now = new Date().getTime();
+    this.timeDifference = this.targetDate.getTime() - now;
+  }
 
-    Math.floor(this.difference) < 10
-      ? (this.weeks.nativeElement.innerText = '0' + Math.floor(this.difference))
-      : (this.weeks.nativeElement.innerText = Math.floor(this.difference));
+  updateCountdown() {
+    const weeks = Math.floor(this.timeDifference / (1000 * 60 * 60 * 24 * 7));
+    const days = Math.floor(
+      (this.timeDifference % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24)
+    );
+    const hours = Math.floor(
+      (this.timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor(
+      (this.timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const seconds = Math.floor((this.timeDifference % (1000 * 60)) / 1000);
 
-    Math.floor(this.difference / 7) < 10
-      ? (this.days.nativeElement.innerText =
-          '0' + Math.floor(this.difference / 7))
-      : (this.days.nativeElement.innerText = Math.floor(this.difference / 7));
+    this.weeks.nativeElement.innerText = this.formatTime(weeks);
+    this.days.nativeElement.innerText = this.formatTime(days);
+    this.hours.nativeElement.innerText = this.formatTime(hours);
+    this.minutes.nativeElement.innerText = this.formatTime(minutes);
+    this.seconds.nativeElement.innerText = this.formatTime(seconds);
+  }
 
-    23 - this.date.getHours() < 10
-      ? (this.hours.nativeElement.innerText = '0' + (23 - this.date.getHours()))
-      : (this.hours.nativeElement.innerText = 23 - this.date.getHours());
-
-    59 - this.date.getMinutes() < 10
-      ? (this.minutes.nativeElement.innerText =
-          '0' + (59 - this.date.getMinutes()))
-      : (this.minutes.nativeElement.innerText = 59 - this.date.getMinutes());
-
-    59 - this.date.getSeconds() < 10
-      ? (this.seconds.nativeElement.innerText =
-          '0' + (59 - this.date.getSeconds()))
-      : (this.seconds.nativeElement.innerText = 59 - this.date.getSeconds());
+  formatTime(time: number): string {
+    return time < 10 ? '0' + time : time.toString();
   }
 }
